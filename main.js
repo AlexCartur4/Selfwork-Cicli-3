7059154ba4e3b81d98e5aaaa183fab33863d1792
Alex Cartura <demonrose66@gmail.com>
2024-09-12 19:25:31 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ while (true) {
   numero = prompt("Inserisci un numero (1, 2 o 3):");
 
   if (numero == 1) {
-    console.log("Hai inserito 1");
+    console.log("E' stata selezionata acqua");
     break;
   } else if (numero == 2) {
-    console.log("Hai inserito 2");
+    console.log("E' stata selezionata coca cola");
     break;
   } else if (numero == 3) {
-    console.log("Hai inserito 3");
+    console.log("E' stata selezionata birra");
     break;
   } else {
     console.log("Numero non valido. Riprova.");
